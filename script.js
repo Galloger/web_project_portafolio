6f420a96b92a030c28a66b04d6febde1986e83b3
German Gallo Poblano <galloger@gmail.com>
2026-04-07 19:17:28 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Scroll Animations
+const sections = document.querySelectorAll('section');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, { threshold: 0.12 });
+  sections.forEach(s => observer.observe(s));
+
+  // Modal Video
+  document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById("videoModal");
+  const openModal = document.getElementById("openVideoModal");
+  const closeModal = document.querySelector(".close-modal");
+  const video = document.getElementById("videoPlayer");
+
+  openModal.onclick = () => {
+    modal.style.display = "flex";
+    video.play();
+  };
+
+  closeModal.onclick = () => {
+    modal.style.display = "none";
+    video.pause();
+    video.currentTime = 0;
+  };
+
+  window.onclick = (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+      video.pause();
+      video.currentTime = 0;
+    }
+  };
+});
